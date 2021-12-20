db1cdf2c5fe5a76d78d6f4274835c1f9f76ddf50
hernays <hernays12@gmail.com>
2021-12-20 20:41:07 -0300

    1.0

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './css/styles.css';
+import './styles.css';
+
+import foto from './assets/img/pm2.png';
+
+var nombre = "hernays";
+
+console.log(nombre);
+
+
+const img = document.createElement('img');
+
+img.src = foto;
+document.body.append(img)
+
+   
